24c75db5d918461a89e8ed8cdf9193c14e90d4c5
CamdenBaucom <camdenbaucom1@gmail.com>
2021-05-18 20:56:12 +0100

    changing letters to numbers

--- rules.py
+++ rules.py
@@ -31,14 +31,17 @@ def show_board():
 		x = z*8
 		for i in range(y,x):
 			board64short.append(board64[i])
-			boardstrstart = "["
-			boardstrend = "]"
-			boardstrmid = ']['.join(map(str, board64short))
+			boardnumbers = 8 - ((i // 8) )
+			stri = str(boardnumbers)
+			boardstrstart = stri + " [ "
+			boardstrend = " ]"
+			boardstrmid = ' ][ '.join(map(str, board64short))
 		boardstrnew = ""
 		boardstrnew = boardstrstart+boardstrmid+boardstrend+"\n"
 		global boardstr
 		boardstr = boardstr+boardstrnew
 		board64short.clear()
+	boardstr = boardstr + "    a    b    c    d    e    f    g    h"
 	print(boardstr)
 	boardstrmid = ""
 	boardstrnew = ""
@@ -74,6 +77,50 @@ def horiz_range(movestart):
 	horiz_range_group_lower = movestart//8
 	horiz_range_group_higher = horiz_range_group_lower+1
 	return range(horiz_range_group_lower*8,horiz_range_group_higher*8)
+
+def board_notation_convtr(movestart):
+	board_notation_char = [char for char in movestart]
+	#board_notation_counter = 0
+	if 'a' in board_notation_char or 'A' in board_notation_char:
+		board_notation_counter = 7
+	elif 'b' in board_notation_char or 'B' in board_notation_char:
+		board_notation_counter = 6
+        elif 'c' in board_notation_char or 'C' in board_notation_char:
+                board_notation_counter = 5
+        elif 'd' in board_notation_char or 'D' in board_notation_char:
+                board_notation_counter = 4
+        elif 'e' in board_notation_char or 'E' in board_notation_char:
+                board_notation_counter = 3
+        elif 'f' in board_notation_char or 'F' in board_notation_char:
+                board_notation_counter = 2
+        elif 'g' in board_notation_char or 'G' in board_notation_char:
+                board_notation_counter = 1
+	elif 'h' in board_notation_char or 'H' in board_notation_char:
+		board_notation_counter = 0
+        else:
+		return False
+        if '1' in board_notation_char:
+        elif '2' in board_notation_char:
+                board_notation_counter += 8
+        elif '3' in board_notation_char:
+                board_notation_counter += 16
+        elif '4' in board_notation_char:
+                board_notation_counter += 24
+        elif '5' in board_notation_char:
+                board_notation_counter += 32
+        elif '6' in board_notation_char:
+                board_notation_counter += 40
+        elif '7' in board_notation_char:
+                board_notation_counter += 48
+	elif '8' in board_notation_char:
+		board_notation_counter += 56
+        else:
+                return False
+
+
+
+
+	return board_notation_char
 
 def can_take(movestart):
 	if board120[(move_convtr_64120(movestart))].islower() == True:
@@ -246,8 +293,9 @@ def play():
 		upd_board_64120()
 
 
-upd_board_12064()
-play()
+#upd_board_12064()
+#play()
+print(board_notation_to_square_numb('A3'))
 
 #print(move_is_legal(61,16))
 #print(hit_detec(37,16))
